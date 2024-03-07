b79a0e0abcd0f906dc3d9c20586b9a772165ffa4
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-07 17:20:33 +0900

    add: 開発環境での定数設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener_web # 送信方法を指定
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 
   config.action_mailer.perform_deliveries = true # メールを実際に送信するかどうかを指定
 
