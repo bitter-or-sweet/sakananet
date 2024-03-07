2a623fb7cecb35c915beece25bc7d77f4ead8b52
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-07 16:50:56 +0900

    add: ルーティングを設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :letter_opener_web # 送信方法を指定
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.perform_deliveries = true # メールを実際に送信するかどうかを指定
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'tops#index'
   resources :users, only: %i[new create]
   resources :password_resets, only: %i[new create edit update]
