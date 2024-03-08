7e9909d6c200517f8640eac59e904262867b8c6b
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-08 22:13:49 +0900

    add: デフォルト言語を日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    #デフォルトの言語を日本語に設定する。
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
