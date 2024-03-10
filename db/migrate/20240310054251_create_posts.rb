9f721e454aa76795c1db124a719b29daf45eb8cc
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-10 17:31:27 +0900

    add: title、bodyカラムにNOTNull制約追加

--- db/migrate/20240310054251_create_posts.rb
+++ db/migrate/20240310054251_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
       t.string :alcohol
       t.string :appetizer
       t.references :user, null: false, foreign_key: true
