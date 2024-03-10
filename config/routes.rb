1142c13135b1d245b1f778576d0570fca6d790da
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-10 18:36:26 +0900

    add: 投稿一覧ページのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'tops#index'
   resources :users, only: %i[new create]
+  resources :posts, only: %i[index]
   resources :password_resets, only: %i[new create edit update]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_06_064742) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_10_054251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.string "alcohol", null: false
+    t.string "appetizer", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_06_064742) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
