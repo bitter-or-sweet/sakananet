8e3b7b61a25011141bd143b6300034bf789ca46d
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-10 17:39:27 +0900

    add: user、postモデルにアソシエーション、バリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 65_535 }
+  validates :alcohol, presence: true, length: { maximum: 255 }
+  validates :appetizer, presence: true, length: { maximum: 255 }
 end
