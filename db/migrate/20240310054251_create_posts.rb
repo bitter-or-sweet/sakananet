9036cc279e5d17f9885a477cbc75843b2d8e53e2
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-10 17:21:19 +0900

    add: postモデル作成

---
+++ db/migrate/20240310054251_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.string :alcohol
+      t.string :appetizer
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
