26b30308e89bc5441d4a55e713b7bb4295af82a6
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-11 16:10:39 +0900

    update: postsコントローラ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  skip_before_action :require_login, only: [index]
+  skip_before_action :require_login, only: %i[index]
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc)
   end
