7e6228eb95a5a6348993b3ca7853a28672a461c5
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-12 10:42:33 +0900

    add: postsコントローラにnew, createアクション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: t('defaults.message.created', item: Post.model_name.human)
+    else
+      flash.now['alert'] = t('defaults.message.not_created', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :alcohol, :appetizer, :body)
+  end
 end
