ef39f8981adc9d7ea4d08e7130088f2d0dbe99e9
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-17 20:57:09 +0900

    add: キャッシュを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :avatar_cache)
   end
 end
