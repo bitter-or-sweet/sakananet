5b7dd21022afc3f973e09013e2bee1ffd29a5a29
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-21 20:58:16 +0900

    add: 投稿編集・削除機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
+  before_action :set_post, only: %i[edit update destroy]
+
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc)
   end
@@ -30,7 +32,27 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user).order(created_at: :desc)
   end
 
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: t('defaults.message.updated', item: Post.model_name.human)
+    else
+      flash.now[:alert] = t('defaults.message.not_updated', item: Post.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to posts_path, notice: t('defaults.message.deleted', item: Post.model_name.human), status: :see_other
+  end
+
   private
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :alcohol, :alcohol_genre_id, :appetizer, :appetizer_genre_id, :body, :post_image, :post_image_cache)
