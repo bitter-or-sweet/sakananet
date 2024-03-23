b0fced62212657784de8ab63d7575ee1d26aed66
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-23 13:06:30 +0900

    add: 投稿一覧、投稿詳細ページのいいね後リダイレクト先を設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,25 @@
 class LikesController < ApplicationController
 
-	def create
-		post = Post.find(params[:post_id])
-		current_user.like(post)
-		redirect_to post_path(post), notice: t('.success')
-	end
+  def create
+    post = Post.find(params[:post_id])
+    current_user.like(post)
+    redirect_after_like(params[:return_to], post)
+  end
 
-	def destroy
-		post = current_user.likes.find(params[:id]).post
-		current_user.unlike(post)
-		redirect_to post_path(post), notice: t('defaults.message.unlike'), status: :see_other
-	end
+  def destroy
+    post = current_user.likes.find(params[:id]).post
+    current_user.unlike(post)
+    redirect_after_like(params[:return_to], post)
+  end
+
+  private
+
+  def redirect_after_like(return_to, post)
+    case return_to
+    when 'index'
+      redirect_to posts_path, notice: t('.success'), status: :see_other
+    when 'show'
+      redirect_to post_path(post), notice: t('.success'), status: :see_other
+    end
+  end
 end
