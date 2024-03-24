e7d070c07a205814d41b611e1c4cf99576b72dff
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-24 13:24:21 +0900

    update: いいねボタンajax化のため、likesコントローラを変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,27 +1,12 @@
 class LikesController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    current_user.like(post)
-    redirect_after_like(params[:return_to], post)
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
   end
 
   def destroy
-    post = current_user.likes.find(params[:id]).post
-    current_user.unlike(post)
-    redirect_after_like(params[:return_to], post)
-  end
-
-  private
-
-  def redirect_after_like(return_to, post)
-    case return_to
-    when 'index'
-      redirect_to posts_path, notice: t('.success'), status: :see_other
-    when 'show'
-      redirect_to post_path(post), notice: t('.success'), status: :see_other
-    when 'liked_index'
-      redirect_to likes_posts_path, notice: t('.success'), status: :see_other
-    end
+    @post = current_user.likes.find(params[:id]).post
+    current_user.unlike(@post)
   end
 end
