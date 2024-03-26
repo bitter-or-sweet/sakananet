e439a1d6ce1d73e2455a487422a1d2b886ff96d3
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-26 23:44:49 +0900

    add: お酒のジャンル、おつまみのジャンルでの検索機能を追加

--- app/models/appetizer_genre.rb
+++ app/models/appetizer_genre.rb
@@ -2,4 +2,8 @@ class AppetizerGenre < ApplicationRecord
   has_many :posts
 
   validates :genre, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "genre", "id", "updated_at"]
+  end
 end
