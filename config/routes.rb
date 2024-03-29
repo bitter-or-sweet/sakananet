80d27788167ad97f97fb2b4613005599046427d2
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-03-29 18:44:32 +0900

    add: プロフィールページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'tops#index'
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
   resources :posts do
     resources :comments, only: %i[create destroy], shallow: true
     collection do
