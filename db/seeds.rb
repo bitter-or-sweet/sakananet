9fd02ffc66ee503147eca40e99e6e7cfff20e14c
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-04-03 21:35:59 +0900

    fix: ジャンル初期データの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-alcohol_genres = ['ビール', '焼酎', '日本酒', 'ウイスキー', 'ワイン', 'その他']
+alcohol_genres = ['ビール', '焼酎', '日本酒', 'ウイスキー', 'ブランデー', 'ワイン', 'ジン', 'ウォッカ', 'ラム', 'テキーラ', '梅酒', 'リキュール', 'サワー', '酎ハイ', 'カクテル', 'その他']
 alcohol_genres.each do |genre|
   AlcoholGenre.find_or_create_by!(genre: genre)
 end
 
-appetizer_genres = ['和食', '洋食', '中華', '韓国', 'イタリアン', 'フレンチ', 'その他']
+appetizer_genres = ['和食', '洋食', '中華', '韓国', 'イタリアン', 'フレンチ', 'スペイン', 'インド', 'タイ', 'メキシカン', 'その他']
 appetizer_genres.each do |genre|
   AppetizerGenre.find_or_create_by!(genre: genre)
 end
