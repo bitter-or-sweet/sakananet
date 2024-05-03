58876e04c4408e70301e623e541ae6d0e9c58f62
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-05-03 13:57:06 +0900

    update: お酒のジャンルをプライベートメソッドに移動

--- app/controllers/appetizers_controller.rb
+++ app/controllers/appetizers_controller.rb
@@ -2,6 +2,7 @@ class AppetizersController < ApplicationController
   ALCOHOL_GENRES = ["ビール", "焼酎", "日本酒", "ウイスキー", "ブランデー", "ワイン", "梅酒", "サワー", "酎ハイ"]
 
   before_action :set_token, only: :create
+  before_action :set_genres, only: %i[new create]
 
   def index
     @appetizers = Appetizer.where(user_id: params[:user_id]).includes(:user).order(created_at: :desc).page(params[:page])
@@ -9,7 +10,6 @@ class AppetizersController < ApplicationController
 
   def new
     @appetizer = Appetizer.new
-    @alcohol_genres = AlcoholGenre.where(genre: ALCOHOL_GENRES)
   end
 
   def create
@@ -27,7 +27,6 @@ class AppetizersController < ApplicationController
       render :new, status: :unprocessable_entity
     rescue StandardError
       flash.now[:alert] = "予期せぬエラーが発生しました。もう一度お試しいただくか、管理者までお問い合わせください。"
-      @alcohol_genres = AlcoholGenre.where(genre: ALCOHOL_GENRES)
       render :new, status: :unprocessable_entity
     end
   end
@@ -44,5 +43,9 @@ class AppetizersController < ApplicationController
 
   def set_token
     @api_key = Rails.application.credentials.dig(:openai, :api_key)
+  end
+
+  def set_genres
+    @alcohol_genres = AlcoholGenre.where(genre: ALCOHOL_GENRES)
   end
 end
