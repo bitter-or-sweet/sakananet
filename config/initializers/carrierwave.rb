3c956e9446989bb52496aae3ca0a79981b982612
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-05-10 22:07:09 +0900

    update: 開発環境で投稿画像が表示されない問題を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,6 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'sakananet' # 作成したバケット名
-    config.asset_host = 'https://sakananet.s3.ap-northeast-1.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
