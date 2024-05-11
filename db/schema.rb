7ec1a726b2e4be2514779cd16abc46d054dd131a
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-05-11 14:16:59 +0900

    update: 投稿の本文を任意入力に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_10_044005) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_11_044921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,7 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_10_044005) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
-    t.text "body", null: false
+    t.text "body"
     t.string "alcohol", null: false
     t.string "appetizer", null: false
     t.bigint "user_id"
