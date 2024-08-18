1974760328a4b915003600a71ca93cf36634d71a
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-08-18 20:06:59 +0900

    add: Userモデルのバリデーションテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it '設定したすべてのバリデーションが機能しているか' do
+      user = User.create(name: "tarou", email: "user@example.com", password: "password", password_confirmation: "password")
+      expect(user).to be_valid
+      expect(user.errors).to be_empty
+    end
+  end
 end
