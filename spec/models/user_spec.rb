adaa0686c8b8be5a945ebf79e70f3585d5c42e95
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-09-04 23:47:52 +0900

    add: Userモデルスペック

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,10 +14,35 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("パスワードは3文字以上で入力してください")
     end
 
-    it '名前が入力されていない場合、バリデーションが機能してinvalidになるか' do
+    it 'password_confirmationが入力されていない場合、バリデーションが機能してinvalidになるか' do
+      user = build(:user, password_confirmation: nil)
+      expect(user).to be_invalid
+      expect(user.errors.full_messages).to include("パスワード確認を入力してください")
+    end
+
+    it 'nameが入力されていない場合、バリデーションが機能してinvalidになるか' do
       user = build(:user, name: nil)
       expect(user).to be_invalid
       expect(user.errors.full_messages).to include("名前を入力してください")
+    end
+
+    it 'nemeが11文字以上の場合、バリデーションが機能してinvalidになるか' do
+      user = build(:user, name: "aaabbbcccddd")
+      expect(user).to be_invalid
+      expect(user.errors.full_messages).to include("名前は10文字以内で入力してください")
+    end
+
+    it 'emailが入力されていない場合、バリデーションが機能してinvalidになるか' do
+      user = build(:user, email: nil)
+      expect(user).to be_invalid
+      expect(user.errors.full_messages).to include("メールアドレスを入力してください")
+    end
+
+    it 'emailが重複した場合、バリデーションが機能してinvalidになるか' do
+      user1 = create(:user, email: "user@example.com")
+      user2 = build(:user, email: user1.email)
+      expect(user2).to be_invalid
+      expect(user2.errors.full_messages).to include("メールアドレスはすでに存在します")
     end
   end
 end
