60ecc05eb3d6038861f7398ec3d718ba0f46d295
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-09-07 21:15:51 +0900

    update: Userモデルスペック

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,37 +8,37 @@ RSpec.describe User, type: :model do
       expect(user.errors).to be_empty
     end
 
-    it 'passwordが3文字未満の場合、バリデーションが機能してinvalidになるか' do
+    it 'passwordが3文字未満の場合は無効' do
       user = build(:user, password: "pa")
       expect(user).to be_invalid
       expect(user.errors.full_messages).to include("パスワードは3文字以上で入力してください")
     end
 
-    it 'password_confirmationが入力されていない場合、バリデーションが機能してinvalidになるか' do
+    it 'password_confirmationが入力されていない場合は無効' do
       user = build(:user, password_confirmation: nil)
       expect(user).to be_invalid
       expect(user.errors.full_messages).to include("パスワード確認を入力してください")
     end
 
-    it 'nameが入力されていない場合、バリデーションが機能してinvalidになるか' do
+    it 'nameが入力されていない場合は無効' do
       user = build(:user, name: nil)
       expect(user).to be_invalid
       expect(user.errors.full_messages).to include("名前を入力してください")
     end
 
-    it 'nemeが11文字以上の場合、バリデーションが機能してinvalidになるか' do
+    it 'nemeが10文字を超える場合は無効' do
       user = build(:user, name: "aaabbbcccddd")
       expect(user).to be_invalid
       expect(user.errors.full_messages).to include("名前は10文字以内で入力してください")
     end
 
-    it 'emailが入力されていない場合、バリデーションが機能してinvalidになるか' do
+    it 'emailが入力されていない場合は無効' do
       user = build(:user, email: nil)
       expect(user).to be_invalid
       expect(user.errors.full_messages).to include("メールアドレスを入力してください")
     end
 
-    it 'emailが重複した場合、バリデーションが機能してinvalidになるか' do
+    it 'emailが重複した場合は無効' do
       user1 = create(:user, email: "user@example.com")
       user2 = build(:user, email: user1.email)
       expect(user2).to be_invalid
