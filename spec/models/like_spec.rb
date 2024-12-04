5cebed060a7fc5f3c356fabc77d56920a9cbb537
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-12-04 22:42:34 +0900

    update: 一部更新

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Like, type: :model do
   let(:user) { create(:user) }
   let(:post) { create(:post) }
 
-  describe "バリデーションチェック" do
-    it "設定した全てのバリデーションが機能しているか" do
+  describe 'バリデーションチェック' do
+    it '設定した全てのバリデーションが機能しているか' do
       like = create(:like, user: user, post: post)
       expect(like).to be_valid
     end
 
-    it "ユーザーが投稿に2回いいねした場合は無効" do
+    it 'ユーザーが投稿に2回いいねした場合は無効' do
       create(:like, user: user, post: post)
       duplicate_like = build(:like, user: user, post: post)
       expect(duplicate_like).to be_invalid
@@ -18,12 +18,12 @@ RSpec.describe Like, type: :model do
     end
   end
 
-  describe "アソシエーションチェック" do
-    it "likeとuserが多対1の関係になっている" do
+  describe 'アソシエーションチェック' do
+    it 'likeとuserが多対1の関係になっている' do
       expect(described_class.reflect_on_association(:user).macro).to eq(:belongs_to)
     end
 
-    it "likeとpostが多対1の関係になっている" do
+    it 'likeとpostが多対1の関係になっている' do
       expect(described_class.reflect_on_association(:post).macro).to eq(:belongs_to)
     end
   end
