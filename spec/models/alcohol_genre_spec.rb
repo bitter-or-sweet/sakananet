cebd28bdee25ad097ae2b4355d96f06de87fcc80
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-12-06 22:53:57 +0900

    add: Alcohol_Genreモデルのバリデーションテスト

---
+++ spec/models/alcohol_genre_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe AlcoholGenre, type: :model do
+	describe 'バリデーションチェック' do
+		it '設定したバリデーションが機能しているか' do
+			alcohol_genre = build(:alcohol_genre)
+			expect(alcohol_genre).to be_valid
+			expect(alcohol_genre.errors).to be_empty
+		end
+
+		it 'genreが入力されていない場合は無効' do
+			genre = build(:alcohol_genre, genre: nil)
+			expect(genre).to be_invalid
+			expect(genre.errors.full_messages).to include("お酒のジャンルを入力してください")
+		end
+	end
+end
