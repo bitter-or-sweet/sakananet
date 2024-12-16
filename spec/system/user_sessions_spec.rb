85fba8fa6400a10811354e61562bec692321f38d
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-12-16 23:11:31 +0900

    add: LoginMacrosを反映

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
+  include LoginMacros
   let(:user) { create(:user) }
 
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する' do
-        visit login_path
-        fill_in 'メールアドレス', with: user.email
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログイン'
+        login(user)
         expect(page).to have_content 'ログインしました'
         expect(current_path).to eq root_path
       end
