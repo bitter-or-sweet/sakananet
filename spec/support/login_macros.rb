2d25ac00c7d2bc50827526c979212b000438d82c
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-12-16 23:07:28 +0900

    update: パスワードを変更

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -2,7 +2,7 @@ module LoginMacros
   def login(user)
     visit login_path
     fill_in 'メールアドレス', with: user.email
-    fill_in 'パスワード', with: user.password
+    fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
   end
 end
