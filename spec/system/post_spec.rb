1cb4bc66483b0a488df480edf517b155c8873f5b
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2024-12-23 23:52:48 +0900

    add: Postのシステムスペック(ログイン後)

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -44,6 +44,7 @@ RSpec.describe 'Posts', type: :system do
 	describe 'ログイン後' do
 		before do
 			login(user)
+			expect(page).to have_content 'ログインしました'
 			create(:alcohol_genre, genre: 'ビール')
 			create(:appetizer_genre, genre: '和食')
 		end
@@ -51,7 +52,6 @@ RSpec.describe 'Posts', type: :system do
 		describe '投稿新規作成' do
 			context 'フォームの入力値が正常' do
 				it '投稿の作成が成功する' do
-					sleep(0.1)
 					visit new_post_path
 					fill_in 'タイトル', with: 'test_title'
 					fill_in 'お酒', with: 'test_content'
@@ -64,6 +64,75 @@ RSpec.describe 'Posts', type: :system do
 					expect(page).to have_content '投稿を作成しました'
 					expect(current_path).to eq posts_path
 				end
+			end
+
+			context 'タイトルが未入力' do
+				it '投稿の新規作成が失敗する' do
+					visit new_post_path
+					fill_in 'タイトル', with: ''
+					fill_in 'お酒', with: 'test_content'
+					select 'ビール', from: 'post[alcohol_genre_id]'
+					fill_in 'おつまみ', with: 'test_content'
+					select '和食', from: 'post[appetizer_genre_id]'
+					click_button '投稿する'
+					expect(page).to have_content '投稿を作成出来ませんでした'
+					expect(page).to have_content 'タイトルを入力してください'
+					expect(current_path).to eq new_post_path
+				end
+			end
+		end
+
+		describe '投稿編集' do
+			let!(:post) { create(:post, user: user) }
+			let(:other_post) { create(:post, user: user) }
+			before { visit edit_post_path(post) }
+
+			context 'フォームの入力値が正常' do
+				it '投稿の編集が成功する' do
+					fill_in 'タイトル', with: 'update_title'
+					click_button '投稿する'
+					expect(page).to have_content 'test_post'
+					expect(page).to have_content 'お酒 ：test_post'
+					expect(page).to have_selector('i.fa-solid.fa-wine-glass')
+					expect(page).to have_content 'おつまみ ：test_post'
+					expect(page).to have_selector('i.fa-solid.fa-utensils')
+					expect(page).to have_content '投稿を更新しました'
+					expect(current_path).to eq post_path(post)
+				end
+			end
+
+			context 'タイトルが未入力' do
+				it '投稿の編集が失敗する' do
+					fill_in 'タイトル', with: nil
+					click_button '投稿する'
+					expect(page).to have_content '投稿を更新出来ませんでした'
+					expect(page).to have_content 'タイトルを入力してください'
+					expect(current_path).to eq edit_post_path(post)
+				end
+			end
+
+			context '他ユーザーの投稿編集ページにアクセス' do
+				let!(:other_user) { create(:other_user) }
+				let!(:other_post) { create(:post, user: other_user) }
+
+				it '編集ページへのアクセスが失敗する' do
+					visit edit_post_path(other_post)
+					expect(page).to have_content '投稿が見つかりません。'
+					expect(current_path).to eq posts_path
+				end
+			end
+		end
+
+		describe '投稿削除' do
+			let!(:post) { create(:post, user: user) }
+
+			it '投稿の削除が成功する' do
+				visit post_path(post)
+				find('.fa-trash-can').click
+				expect(page.accept_confirm).to eq '削除しますか？'
+				expect(page).to have_content '投稿を削除しました'
+				expect(current_path).to eq posts_path
+				expect(page).not_to have_content post.title
 			end
 		end
 	end
