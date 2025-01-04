e409deee46cd0409c23db1708fc96d7a08b44d4f
rq-bitter-or-sweet-at-321899771753 <xiongdengangang@gmail.com>
2025-01-04 10:10:34 +0900

    add: NotificationモデルとLikeモデル、Commentモデルとの関連付け

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  has_one :notification, as: :notifiable, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 100 }
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  has_one :notification, as: :notifiable, dependent: :destroy
+
   validates :user_id, uniqueness: { scope: :post_id }
 
   after_create_commit :create_like_notification
